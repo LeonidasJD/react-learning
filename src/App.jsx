c12be41c077624e567cf95ff79c5d8586e0d2c66
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-22 09:58:27 +0100

    kreiranje visenamneske komponente koriscenjemem propsa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImage from "./assets/components.png";
+
 function Header() {
   const description = reactDescriptions[getRandomInt(2)];
 
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
 
       <p>
@@ -14,12 +17,34 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.description} />
+      <h3>{props.title}</h3> {/* koriscinje vrednosti propsa */}
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components" //kreiranje vrednosti komponente pomocu PROPS-A. TITLE, DESCRIPTION,IMG SU PROPSOVI.
+              description="The core UI building block"
+              img={componentsImage}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
