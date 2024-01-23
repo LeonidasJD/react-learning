259e0aeabdee74f03f0d39c9dc5e3c1959bba40b
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-23 21:57:35 +0100

    nista vaznoo

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -2,7 +2,7 @@ export default function TabButton({children,onSelect}){/*koriscen je children pr
                                                 umesto da smo pisali props, pa u buttonu props.children iskoristili smo laksi metod gde smo u 
                                                 viticastim zagradama stavili taj props children*/
     return(
-       <li><button onClick={onSelect}>{ children}</button></li> /*onSelect jeste props koji prihvata funkciju koja je kreirana u komponenti gde je pozvana TabButton komponenta */
+       <li><button onClick={onSelect}>{children}</button></li> /*onSelect jeste props koji prihvata funkciju koja je kreirana u komponenti gde je pozvana TabButton komponenta */
     );
 
     
