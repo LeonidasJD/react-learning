562ec29d5568b9cbea8665a080c17bd7a0a45d0e
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-23 21:28:02 +0100

    importovan css za komponentu

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,9 +1,13 @@
+import "./CoreConcept.css";// importovan css fajl koji stilizuje CoreConcept komponentu 
+
 export default function CoreConcept(props) {
   return (
     <li>
       <img src={props.img} alt={props.description} />
       <h3>{props.title}</h3> {/* koriscinje vrednosti propsa */}
       <p>{props.description}</p>
+      
     </li>
+    
   );
 }
