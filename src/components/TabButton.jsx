1d522c20903e8d521c9939c03c64822a5d677f82
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-23 21:28:44 +0100

    kreirana komponenta tabbuton

---
+++ src/components/TabButton.jsx
@@ -0,0 +1,9 @@
+export default function TabButton({children,onSelect}){/*koriscen je children props , koji nam omogucava da unutar komponente dodajemo elemente
+                                                umesto da smo pisali props, pa u buttonu props.children iskoristili smo laksi metod gde smo u 
+                                                viticastim zagradama stavili taj props children*/
+    return(
+       <li><button onClick={onSelect}>{ children}</button></li> /*onSelect jeste props koji prihvata funkciju koja je kreirana u komponenti gde je pozvana TabButton komponenta */
+    );
+
+    
+}
