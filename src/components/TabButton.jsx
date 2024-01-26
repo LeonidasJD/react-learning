c816ed89a9f8c20c23830f440ba51a8333fa6f69
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-26 22:01:53 +0100

    dynamic css

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,9 +1,10 @@
-export default function TabButton({children,onSelect}){/*koriscen je children props , koji nam omogucava da unutar komponente dodajemo elemente
+export default function TabButton({children,onSelect,isSelected}){/*koriscen je children props , koji nam omogucava da unutar komponente dodajemo elemente
                                                 umesto da smo pisali props, pa u buttonu props.children iskoristili smo laksi metod gde smo u 
                                                 viticastim zagradama stavili taj props children*/
     return(
-       <li><button onClick={onSelect}>{children}</button></li> /*onSelect jeste props koji prihvata funkciju koja je kreirana u komponenti gde je pozvana TabButton komponenta */
-    );
+       <li><button className={isSelected ?  "active" : undefined} onClick={onSelect}>{children}</button></li> /*onSelect jeste props koji prihvata funkciju koja je kreirana u komponenti gde je pozvana TabButton komponenta */
+    );/**isSelected props smo kreirali zbog dinamicke klase. Ako tamo gde pozivamo komponentu TabButton postoji isSelected klasa "active"
+    ce se primeniti na button ako ne postoji onda nece */
 
     
 }
