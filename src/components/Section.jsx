577490626f141cf5f2285e4e314ed85b8d01641e
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-29 21:22:00 +0100

    nova komponenta

---
+++ src/components/Section.jsx
@@ -0,0 +1,8 @@
+export default function Section({text,children, ...props}){
+    return(
+        <section {...props}>
+            <h2>{text}</h2>
+            {children}
+        </section>
+    );
+}
