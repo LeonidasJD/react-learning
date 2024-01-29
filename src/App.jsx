dce4797183da32db49703273decc92084a638370
LeonidasJD <milandjordjevic633@gmail.com>
2024-01-29 21:20:54 +0100

    component split

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,20 @@
 import Header from "./components/Header/Header.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { useState,Fragment } from "react";
-import { EXAMPLES } from "./data.js";
+import { Fragment } from "react";
 import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
+
 
 
 function App() {
-  let [selectedTopic, setSelectedTopic] = useState(); // SELECTED TOPIC JESTE POCETNO STANJE KOJE SE PRIKAZUJE A KADA IZVRSIMO FUNKCIJU handleClick novu vrednost pakujemo u setSelectedTopic
-
-
-  function handleClick(selectedButton) {
-    //selectedButton=> 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);
-  }
+ 
 
   return (
     <Fragment>
       <Header />
-      <main>
-        
-      <CoreConcepts />
-        <section id="examples">
-          <h2>Examples</h2>
-          <menu>
-            <TabButton isSelected={selectedTopic === "components"} onSelect={() => handleClick("components")}>
-              Components
-            </TabButton> 
-            <TabButton isSelected={selectedTopic === "jsx"} onSelect={() => handleClick("jsx")}>JSX</TabButton>{/**proveravamo da li je stanje jsx ako jeste onda je is selected true i postavlja se klasa "active na dugme" */}
-            <TabButton isSelected={selectedTopic === "props"} onSelect={() => handleClick("props")}>Props</TabButton>
-            <TabButton isSelected={selectedTopic === "state"} onSelect={() => handleClick("state")}>State</TabButton>
-          </menu>
-        <div>
-          {!selectedTopic ? <p>Please selec a topic!</p> : null}
 
-          {selectedTopic ? <div id="tab-content">
-            <p>{EXAMPLES[selectedTopic].title}</p>
-            <pre>{EXAMPLES[selectedTopic].description}</pre>
-            <code>{EXAMPLES[selectedTopic].code}</code>
-          </div> : null}
-          {/**POCETNO STANJE useState-a je selectedTopic */}
-          </div>
-        </section>
-        
+      <main>
+      <CoreConcepts />
+       <Examples />
       </main>
       
     </Fragment>
